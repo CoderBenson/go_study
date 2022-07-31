76acb6953a53ce125c81d37900cd2a6fdfcc70d2
CoderBenson <benson.csu@gmail.com>
2022-07-31 23:13:48 +0800

    zookeeper getw/exitsw/childw

--- middleware/zookeeper/get_set.go
+++ middleware/zookeeper/get_set.go
@@ -50,11 +50,19 @@ func create(path, value string) (string, error) {
 
 func get(path string) (string, error) {
 	conn := ObtainConn()
+	RecycleConn(conn)
 	result, _, err := conn.Get(path)
 	if err != nil {
 		return "", err
 	}
 	return string(result), nil
+}
+
+func getW(path string) (string, <-chan zk.Event, error) {
+	conn := ObtainConn()
+	defer RecycleConn(conn)
+	r, _, rChan, err := conn.GetW(path)
+	return string(r), rChan, err
 }
 
 func exists(path string) (bool, error) {
@@ -64,21 +72,23 @@ func exists(path string) (bool, error) {
 	return r, err
 }
 
-func set(path, value string) error {
+func existsw(path string) (bool, <-chan zk.Event, error) {
 	conn := ObtainConn()
 	defer RecycleConn(conn)
-	var flags int32 = 0
-	_, err := conn.Set(path, []byte(value), flags)
-	return err
+	r, _, rChan, err := conn.ExistsW(path)
+	return r, rChan, err
 }
 
-const (
-	Operate_Create = "create"
-	Operate_Get    = "get"
-	Operate_Set    = "set"
-	Operate_Exist  = "exist"
-	Operate_Child  = "child"
-)
+func set(path, value string) (string, error) {
+	conn := ObtainConn()
+	defer RecycleConn(conn)
+	old, state, err := conn.Get(path)
+	if err != nil {
+		return "", nil
+	}
+	_, err = conn.Set(path, []byte(value), state.Version)
+	return string(old), err
+}
 
 func child(path string) ([]string, error) {
 	conn := ObtainConn()
@@ -86,6 +96,25 @@ func child(path string) ([]string, error) {
 	cs, _, err := conn.Children(path)
 	return cs, err
 }
+
+func childw(path string) ([]string, <-chan zk.Event, error) {
+	conn := ObtainConn()
+	defer RecycleConn(conn)
+
+	cs, _, csChann, err := conn.ChildrenW(path)
+	return cs, csChann, err
+}
+
+const (
+	Operate_Create = "create"
+	Operate_Get    = "get"
+	Operate_Getw   = "getw"
+	Operate_Set    = "set"
+	Operate_Exist  = "exist"
+	Operate_Existw = "existw"
+	Operate_Child  = "child"
+	Operate_Childw = "childw"
+)
 
 func main() {
 	operateStr := &os.Args[1]
@@ -113,23 +142,74 @@ func main() {
 		} else {
 			fmt.Printf("get from zookeeper successful:%s\n", result)
 		}
+	case Operate_Getw:
+		if r, rChan, err := getW(*pathStr); err != nil {
+			panic(err)
+		} else {
+			fmt.Printf("get from zookeeper successful:%s\n", r)
+			event := <-rChan
+			if err = event.Err; err != nil {
+				panic(event.Err)
+			} else if event.Type == zk.EventNodeDataChanged {
+				r, err = get(*pathStr)
+				if err != nil {
+					panic(err)
+				} else {
+					fmt.Printf("getw channel watch:%s->%s\n", *pathStr, r)
+				}
+			}
+		}
 	case Operate_Exist:
 		if r, err := exists(*pathStr); err != nil {
 			panic(err)
 		} else {
 			fmt.Printf("%s %v exists in zookeeper\n", *pathStr, r)
 		}
-	case Operate_Set:
-		if err := set(*pathStr, *valueStr); err != nil {
+	case Operate_Existw:
+		if r, rChan, err := existsw(*pathStr); err != nil {
 			panic(err)
 		} else {
-			fmt.Printf("set node successful:%s->%s\n", *pathStr, *valueStr)
+			fmt.Printf("%s %v existsw in zookeeper\n", *pathStr, r)
+			event := <-rChan
+			if err = event.Err; err != nil {
+				panic(err)
+			} else if event.Type == zk.EventNodeCreated {
+				str, err := get(*pathStr)
+				if err != nil {
+					panic(err)
+				} else {
+					fmt.Printf("existsw in zookeeper channel watch:%s->%s\n", *pathStr, str)
+				}
+			}
+		}
+	case Operate_Set:
+		if old, err := set(*pathStr, *valueStr); err != nil {
+			panic(err)
+		} else {
+			fmt.Printf("set node(%s) successful:%s->%s\n", old, *pathStr, *valueStr)
 		}
 	case Operate_Child:
 		if cs, err := child(*pathStr); err != nil {
 			panic(err)
 		} else {
 			fmt.Printf("get %s's child from zookeeper successful:%v\n", *pathStr, cs)
+		}
+	case Operate_Childw:
+		if cs, csChan, err := childw(*pathStr); err != nil {
+			panic(err)
+		} else {
+			fmt.Printf("get %s's child from zookeeper successful:%v\n", *pathStr, cs)
+			event := <-csChan
+			if err = event.Err; err != nil {
+				panic(err)
+			} else if event.Type == zk.EventNodeChildrenChanged {
+				cs, err = child(*pathStr)
+				if err != nil {
+					panic(err)
+				} else {
+					fmt.Printf("get %s child from zookeeper channel watch:%v\n", *pathStr, cs)
+				}
+			}
 		}
 	default:
 		panic(fmt.Errorf("%s operate not support", *operateStr))
